b3b0eb9e8afe44eac3ca29fd84d37dee449b06d0
jobe95 <jonatanbengtsson95@hotmail.se>
2019-12-07 20:09:30 +0100

    First

---
+++ Labb4/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Labb4
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Currency { get; set; }
+        public double Population { get; set; }
+        public string Picture { get; set; }
+        public string Description { get; set; }
+
+        public Country(string name, string currency, double population, string picture, string description)
+        {
+            this.Name = name;
+            this.Currency = currency;
+            this.Population = population;
+            this.Picture = picture;
+            this.Description = description;
+        }
+    }
+}
---
+++ Labb4/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Labb4
+{
+    public class MainViewModel : SimpleViewModel
+    {
+        private readonly List<Country> listOfCountries;
+        private Country currentCountry;
+        private int indexCounter = 0;
+        public ICommand NextCountryCommand { get; private set; }
+        public ICommand PreviousCountryCommand { get; private set; }
+        public MainViewModel()
+        {
+            CountryRepository countryRepository = new CountryRepository();
+            listOfCountries = countryRepository.GetCountries();
+            CurrentCountry = listOfCountries[0];
+
+            NextCountryCommand = new Command(nextButtonPressed);
+            PreviousCountryCommand = new Command(previousButtonPressed);
+            
+        }
+
+        public Country CurrentCountry
+        {
+            get {
+                return currentCountry;
+            }
+            set
+            {
+                SetPropertyValue(ref currentCountry, value);
+            }
+        }
+
+        public void nextButtonPressed()
+        {
+            Console.WriteLine("NÄSTA");
+            indexCounter++;
+            if (indexCounter >= listOfCountries.Count)
+            {
+                indexCounter = 0;
+            }
+            CurrentCountry = listOfCountries[indexCounter];
+        }
+
+        private void previousButtonPressed()
+        {
+            Console.WriteLine("BACKA");
+            indexCounter--;
+            if (indexCounter < 0)
+            {
+                indexCounter = listOfCountries.Count - 1;
+                Console.WriteLine(listOfCountries[indexCounter].Name);
+            }
+            CurrentCountry = listOfCountries[indexCounter];
+        }
+    }
+}
